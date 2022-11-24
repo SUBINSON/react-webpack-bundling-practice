45cc85f5a07399e8831a9a9ddd8de59588d641a6
SUBINSON <fraisecheris@gmail.com>
2022-11-24 13:44:42 +0900

    feat: add mini-css-extract-plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,39 +1,42 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-  mode: "development",
+  mode: 'development',
   entry: {
-    app: "./src/index.js",
+    app: './src/index.js',
   },
   resolve: {
-    extensions: ['.js', '.jsx']
+    extensions: ['.js', '.jsx'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: "development",
-      template: './public/index.html' //이거 작성 꼭 할 것. 안하면 에러남
+      title: 'development',
+      template: './public/index.html', //이거 작성 꼭 할 것. 안하면 에러남
     }),
+    new MiniCssExtractPlugin(),
   ],
-  module: { //모듈 연결 설정
+  module: {
+    //모듈 연결 설정
     rules: [
       {
         test: /\.js$/, // 대상 설정 정규식
-        loader: "babel-loader",
+        loader: 'babel-loader',
         options: {
-          presets: ["@babel/preset-env", "@babel/preset-react"],
+          presets: ['@babel/preset-env', '@babel/preset-react'],
         },
       },
       {
         test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
         exclude: /node_modules/,
       },
     ],
   },
   output: {
-    filename: "bundle.js",
-    path: path.resolve(__dirname, "docs"), //깃허브 배포 위해 docs 설정
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'docs'), //깃허브 배포 위해 docs 설정
     clean: true,
   },
 };
